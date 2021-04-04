81193d76480c25d55130086ab421b6bf7b0f976b
Swaraj <swarajrajpure@gmail.com>
2021-04-04 20:25:14 +0530

    adds cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 
 const app = express();
 
 app.use(express.json());
-
-const PORT = process.env.PORT || 4000;
+app.use(cors());
 
 app.use('/', indexRouter);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
