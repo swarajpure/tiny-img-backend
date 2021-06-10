f75e3f40857ba3cf2ef0d6e7155c8c1d10e5158f
Swaraj <swarajrajpure@gmail.com>
2021-06-10 11:03:44 +0530

    feat: make the dimensions half of given image and delete original file

--- controllers/upload.js
+++ controllers/upload.js
@@ -25,9 +25,11 @@ const resize = async (req, res, next) => {
   const originalFile = join(IMAGE_SAVE_LOCATION, originalname);
   const resizedFile = join(RESIZED_IMAGE_LOCATION, originalname);
 
-  // TODO: Change only width or height
-  await sharp(originalFile)
-    .resize(320, 240)
+  const inputImage = sharp(originalFile);
+  const imageMetaData = await inputImage.metadata();
+
+  await inputImage
+    .resize(Math.round(imageMetaData.width / 2))
     .toFile(resizedFile, (err, info) => {
       if (err) console.log(err);
       if (info) console.log('info', info);
@@ -36,7 +38,7 @@ const resize = async (req, res, next) => {
 
   fs.unlink(originalFile, (err) => {
     if (err) console.log(err);
-    console.log('file deleted');
+    console.log('Original file deleted');
   });
   next();
 };
--- utils/imgur.js
+++ utils/imgur.js
@@ -2,19 +2,25 @@
 const { ImgurClient } = require('imgur');
 const config = require('config');
 const { dirname, join } = require('path');
+const fs = require('fs');
 
 const PROJECT_PATH = dirname(__dirname);
 
 const { oauth: { clientId } } = config.get('imgur');
 const client = new ImgurClient({ clientId });
 
-// TODO: delete original image after uploading
 const uploadToImgur = async (req, res) => {
   try {
     const { originalname } = req.file;
     const resizedImage = join(PROJECT_PATH, 'resized', originalname);
 
     const response = await client.upload(resizedImage);
+
+    fs.unlink(resizedImage, (err) => {
+      if (err) console.log(err);
+      console.log('Resized file deleted');
+    });
+
     return res.json({
       link: response.data.link,
       message: 'Successfully resized and uploaded the image!',
